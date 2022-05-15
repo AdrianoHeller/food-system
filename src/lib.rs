375402d10801c5db94e6710abeff5d58d35d5313
adrianohellermylla <adrianohellermylla@gmail.com>
2022-05-14 23:10:29 -0300

    feat: Add impl on Establishment scope

--- src/lib.rs
+++ src/lib.rs
@@ -33,12 +33,10 @@ pub mod infrastructure {
 }
 
 pub mod scope {
-
         pub enum Level {
             Amateur,
             Professional,
         }
-
         pub enum FoodType {
             Asian,
             Chinese,
@@ -49,7 +47,6 @@ pub mod scope {
             Fastfood,
             Other,
         }
-
         pub enum EstabType {
             Restaurant,
             Bar,
@@ -58,16 +55,31 @@ pub mod scope {
             Healthy,
             Other(String),
         }
-        struct Establishment {
-            name: &'static str,
-            cnpj: &'static str,
-            address: &'static str,
-            phone: &'static str,
-            number_of_employees: u16,
+        pub struct Establishment {
+            pub name: &'static str,
+            pub cnpj: &'static str,
+            pub address: &'static str,
+            pub phone: &'static str,
+            pub number_of_employees: u16,
             chef: &'static str,
             estab_type: EstabType,
             food_style: FoodType,
             estab_level: Level,
         }
+    impl Establishment {
+        pub fn new(name: &'static str, cnpj: &'static str, address: &'static str, phone: &'static str,number_of_employees: u16) -> Establishment {
+            Establishment {
+                name,
+                cnpj,
+                address,
+                phone,
+                number_of_employees,
+                chef: "John Connor",
+                estab_type: EstabType::Restaurant,
+                food_style: FoodType::Asian,
+                estab_level: Level::Professional,
+            }
+        }
+    }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use kitchen::infrastructure;
+use kitchen::{infrastructure,scope};
 
 fn main() {
     let funcionario = infrastructure::Employer::new(
         String::from("Adriano"),
         String::from(" Heller Mylla"),
         String::from("devops.heller@gmail.com"));
+
+    let t = scope::
 
     println!("{:#?}",funcionario);
 }
